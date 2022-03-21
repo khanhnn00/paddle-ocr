5333d0859fc0d12f7303a750256d0d4730ca008a
Khanhnn00 <18520901@gm.uit.edu.vn>
2022-03-21 17:40:28 +0700

    add train_det.sh

---
+++ train_rec.sh
@@ -0,0 +1,2 @@
+# recommended paddle.__version__ == 2.0.0
+python3.8 -m paddle.distributed.launch --log_dir=./debug/ --gpus '0' tools/train.py -c configs/rec/multi_language/rec_en_number_lite_finetune.yml

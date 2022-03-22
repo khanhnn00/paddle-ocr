4ce7ae97c803427e44254565efe6697b601373e4
khanhnn <khanhnguyenngoc@channelnet.vn>
2022-03-22 16:59:03 +0700

    xD

--- infer_full.sh
+++ infer_full.sh
@@ -1,2 +1,2 @@
 # Predict English results
-python3.8 infer_full.py -c configs/full.yml -o Rec.Global.pretrained_model=output/rec_en_number_lite_finetune/best_accuracy Det.Global.load_static_weights=false Rec.Global.load_static_weights=false Det.Global.infer_img=../dataset/corner_dataset/images/test/stage0_00118.jpg
+python3.8 infer_full.py -c configs/full.yml -o Rec.Global.pretrained_model=output/rec_en_number_lite_final/best_accuracy Det.Global.pretrained_model=output/det_mobile_v2.0/best_accuracy Det.Global.load_static_weights=false Rec.Global.load_static_weights=false Det.Global.infer_img=../dataset/corner_dataset/images/train/crawl_00028.jpg
--- train_det.sh
+++ train_det.sh
@@ -1,2 +1,2 @@
 # recommended paddle.__version__ == 2.0.0
-python3.8 -m paddle.distributed.launch --log_dir=./debug/ --gpus '0,1' tools/train.py -c configs/det/ch_ppocr_v2.0/ch_det_mv3_db_v2.0.yml
+python3.8 -m paddle.distributed.launch --log_dir=./debug2/ --gpus '1' tools/train.py -c configs/det/ch_ppocr_v2.0/ch_det_mv3_db_v2.0.yml
